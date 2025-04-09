31e20acbd6049adfc3f35b8213d3d497ba5f612c
María Pilar Alzuet <alzuet@gmail.com>
2025-04-09 21:04:16 +0200

    Más traducciones, quitar versión y poner tag 1.0.2

--- actualiza.sh
+++ actualiza.sh
@@ -1,5 +1,5 @@
 git add .
-git commit -m "Poner tag 1.0.1"
-git tag v1.0.1
-git push origin v1.0.1
+git commit -m "Más traducciones, quitar versión y poner tag 1.0.2"
+git tag v1.0.2
+git push origin v1.0.2
 git push origin main
